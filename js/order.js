2bf00804ff0bd9be9ef8d96b3346cd2007f022d4
oscarmorgan1 <oscar.morgan001@gmail.com>
2022-08-25 11:24:51 +1000

    Menu Pull
    New tiles are generated for each item in the active menu burger database for each store.

--- js/order.js
+++ js/order.js
@@ -3,6 +3,8 @@ var cartTotal = cart.length
 var cartUI
 var selectedStore = "FBC0001"
 var selectedItem = "Planino Burger"
+var totalToCreate;
+    var index = 1;
 
 
 console.log(sessionStorage.getItem("Order ID"))
@@ -28,26 +30,77 @@ function pullMenu(menuType) {
         console.log(menu.val())
         activeMenu = menu.val()
         console.log(activeMenu.length);
+
+        totalToCreate = activeMenu.length;
+console.log(totalToCreate)
+console.log(index)
+        
+        createItem()
     } 
 
 }
 
+
+
 function createItem() {
 
-    var totalToCreate = activeMenu.length;
-    var index = 1;
+    
 
     if (index < totalToCreate) {
         //create div
-        console.log(activeMenu[1])
-        // create elemements
-
-        //update prices
-        index + 1
-    } else if (index + 1 == totalToCreate) {
-
+        createAndFill()      
+    } else if (index == totalToCreate) {
+       createAndFill()
+    } else {
+        console.log("called stack")
     }
 
+   
+}
+
+
+function createAndFill() {
+    console.log(activeMenu[1])
+    console.log("HELLO")
+
+    var itemsection = document.getElementById("items")
+
+    var newDIV = document.createElement('div');
+    itemsection.appendChild(newDIV)
+    newDIV.className = "column-4 tile grow project-3"
+    // create elemements
+
+    var newImage = document.createElement('img');
+    newDIV.appendChild(newImage)
+    newImage.className = "image"
+    newImage.src = "/images/product_shots/burger.png"
+
+
+    var newTitle = document.createElement('h2');
+    newDIV.appendChild(newTitle)
+    newTitle.className = "item-title"
+
+    database.ref("stores/" + storeID + "/menu/active/" + index).on('value', displayMenuTotal);
+        function displayMenuTotal(menu){
+            console.log(menu.val())
+
+        }
+
+    newTitle.innerHTML = "BURGER TITLE"
+
+    var newDescription = document.createElement('h2');
+    newDIV.appendChild(newDescription)
+    newDescription.className = "item-description"
+    newDescription.innerHTML = "Our most basic, simple and boring burger. Yet packs on falvour."
+
+    var newButton = document.createElement('button');
+    newDIV.appendChild(newButton)
+    newButton.className = "item-order"
+    newButton.innerHTML = "Order"
+    //update prices
+    index = index + 1
+    console.log(index)
+    createItem()
 }
 
 function addtoCart() {
